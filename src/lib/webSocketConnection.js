683de1f13e89683dbfa2032e459c5674913d0f9a
Vuferen <vuferen@gmail.com>
2023-03-08 11:15:21 +0100

    gh-pages script

--- src/lib/webSocketConnection.js
+++ src/lib/webSocketConnection.js
@@ -1,6 +1,6 @@
 import ioClient from 'socket.io-client';
 // const ENDPOINT = 'https://bc3e-130-225-198-168.eu.ngrok.io';
-const ENDPOINT = 'https://project6-peach.vercel.app/';
+const ENDPOINT = 'https://vuferen.github.io/';
 
 const socket = ioClient(ENDPOINT);
 
