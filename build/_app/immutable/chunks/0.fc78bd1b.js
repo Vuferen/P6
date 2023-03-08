9d4693aad5088a9ac09acf343a8a6e0a9d8c944c
Vuferen <vuferen@gmail.com>
2023-03-08 13:13:54 +0100

    Removed build folder, changed host to vercel

--- build/_app/immutable/chunks/0.fc78bd1b.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_layout.svelte.3f8e1df3.js";export{t as component};
--- build/_app/immutable/chunks/1.4b2cfa1e.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.b853d7f8.js";export{t as component};
