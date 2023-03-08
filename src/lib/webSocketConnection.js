2c074c8bef727850f7910e14699ad46f293a233e
Vuferen <vuferen@gmail.com>
2023-03-08 09:57:29 +0100

    Changed socket io endpoint to netlify

--- src/lib/webSocketConnection.js
+++ src/lib/webSocketConnection.js
@@ -1,6 +1,6 @@
 import ioClient from 'socket.io-client';
 // const ENDPOINT = 'https://bc3e-130-225-198-168.eu.ngrok.io';
-const ENDPOINT = 'localhost:3000';
+const ENDPOINT = 'https://beautiful-sable-5b4298.netlify.app/';
 
 const socket = ioClient(ENDPOINT);
 
